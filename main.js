a89f3cc9fe8fc8c26b5155a6de17b4a739485240
DAVID0205J <jdaceituno@scl.edu.gt>
2024-03-07 13:39:58 -0600

    juego de cartas terminado

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-let cards1= ["🥸","👽","🥱","😎","🫢","😏","🙂","🙃","😮‍💨","😡","🤯","🫣"]
+let cards1= ["🤩","🤗","🥱","😎","🫢","😏","🙂","🙃","😮‍💨","😡","🤯","🫣"]
 
-let cards2= ["🥸","👽","🥱","😎","🫢","😏","🙂","🙃","😮‍💨","😡","🤯","🫣"]
+let cards2= ["🤩","🤗","🥱","😎","🫢","😏","🙂","🙃","😮‍💨","😡","🤯","🫣"]
 
 let todas_las_tarjetas = cards1.concat(cards2);
 
@@ -8,10 +8,11 @@ function repartir_cartas(){
    let tablero = document.querySelector("#root");
    
    todas_las_tarjetas.forEach((cada_carta)=> {
-      let cart = document.createElement("div")
-   cart.innerHTML = `<div class= "carta">${cada_carta}</div>`
+      let carta = document.createElement("div")
+      carta.classList.add("carta_tracera")
+   carta.innerHTML = `<div class="carta_fontral">${cada_carta}</div>`
 
-   tablero.appendChild(cart);})
+   tablero.appendChild(carta);})
    
 
    
@@ -19,3 +20,5 @@ function repartir_cartas(){
 }
 
 repartir_cartas();
+
+
